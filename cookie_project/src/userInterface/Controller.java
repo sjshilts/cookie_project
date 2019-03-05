e122e2726d871ac7fdcb4373c44a01909712244d
sjshilts <shilts.sj@gmail.com>
2019-03-05 18:06:38 -0500

    Changed graph to a bar chart

--- cookie_project/src/userInterface/Controller.java
+++ cookie_project/src/userInterface/Controller.java
@@ -3,6 +3,9 @@ package userInterface;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 
 import java.io.IOException;
@@ -14,6 +17,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -21,8 +25,10 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import type.*;
+import javafx.scene.chart.XYChart;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable{
 	
 	@FXML PieChart outflowPieChart;
 	
@@ -35,8 +41,11 @@ public class Controller {
 	@FXML Label account_amount;
 	@FXML Button entry;
 	
-	@FXML
-	public void initialize(){
+	@FXML private BarChart<?,?> costSpending;
+	@FXML private CategoryAxis time;
+	@FXML private NumberAxis amounts;
+	@Override
+	public void initialize(URL url, ResourceBundle rb){
         
         ObservableList<PieChart.Data> in = FXCollections.observableArrayList(
         		new PieChart.Data("Food",50),
@@ -54,7 +63,18 @@ public class Controller {
         ObservableList<Data> tableData = FXCollections.observableArrayList(
 
         		);
-        String amount = "$";
+        
+        XYChart.Series dataIncome = new XYChart.Series<>();
+        dataIncome.setName("Income");
+        dataIncome.getData().add(new XYChart.Data<>("January", 545.43));
+        dataIncome.getData().add(new XYChart.Data<>("Febuary", 618.3));
+        costSpending.getData().addAll(dataIncome);
+        
+        XYChart.Series dataSpending = new XYChart.Series<>();
+        dataSpending.setName("Spending");
+        dataSpending.getData().add(new XYChart.Data<>("January", 394.54));
+        dataSpending.getData().add(new XYChart.Data<>("Febuary", 456.78));
+        costSpending.getData().addAll(dataSpending);
         
         outflowPieChart.setData(in);
         inflowPieChart.setData(out);
