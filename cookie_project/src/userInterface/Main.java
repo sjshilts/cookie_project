30d7d6994fe26ec224fe546e8ec47d6fe5e74e7c
sjshilts <shilts.sj@gmail.com>
2019-03-23 16:19:47 -0400

    Login Screen update

--- cookie_project/src/userInterface/Main.java
+++ cookie_project/src/userInterface/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("userInterface.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
         primaryStage.setTitle("MMM...");
-        primaryStage.setScene(new Scene(root, 1025.0000999999975, 768.0000999999975));
+        primaryStage.setScene(new Scene(root, 485, 612.0000999999975));
         primaryStage.getIcons().add(new Image("/images/cookie_icon.png"));
         primaryStage.show();
     }
