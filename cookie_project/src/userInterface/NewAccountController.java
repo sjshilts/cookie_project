ebbb12704037da1e7f584182b95c0e12e0212f0a
sjshilts <shilts.sj@gmail.com>
2019-03-27 14:02:07 -0400

    Creating a new user account and login work

--- cookie_project/src/userInterface/NewAccountController.java
+++ cookie_project/src/userInterface/NewAccountController.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import java.time.LocalDate;
@@ -33,17 +34,77 @@ public class NewAccountController implements Initializable{
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
 	}
 	
-	public void createNewAccount(ActionEvent event) {
-		// Check if a field is empty
+	public void createNewAccount(ActionEvent event) throws SQLException {
 		
+		// create db connection
+		dbConnect db = new dbConnect( );
+		Connection conn = db.connect( "sjshilts", "sJSdbPass10" );
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String stmt;
+		float bal;
+		
+		// Check if a field is empty
+		if(username.getText().equals("")) {
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Please enter a username");
+			errorAlert.showAndWait();
+			return;
+		}
+		else if(password.getText().equals("")) {
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Please enter a password");
+			errorAlert.showAndWait();
+			return;
+		}
+		else if(!passwordVerify.getText().equals(password.getText())) {
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Password does not match verified password");
+			errorAlert.showAndWait();
+			return;
+		}
+		else if(firstName.getText().equals("")) {
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Please enter a first name");
+			errorAlert.showAndWait();
+			return;
+		}
+		else if(lastName.getText().equals("")) {
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Please enter a last name");
+			errorAlert.showAndWait();
+			return;
+		}
+		else if(initBal.getText().equals("")) {
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Please enter a balance");
+			errorAlert.showAndWait();
+			return;
+		}
+		try{
+			bal = Float.parseFloat(initBal.getText()) - 1;
+			} catch(NumberFormatException e) {
+				Alert errorAlert = new Alert(AlertType.INFORMATION);
+				errorAlert.setHeaderText("Please enter a balance");
+				errorAlert.showAndWait();
+				return;
+			}
 		
 		// input data into database
+		stmt = "INSERT INTO Users (username, password, firstname, lastname, InitBalance) VALUES ( ?, ?, ?, ?, ? )";
+		ps = conn.prepareStatement( stmt );
+		ps.setString( 1, username.getText() );									//set username
+		ps.setString( 2, passwordVerify.getText() );							//set password
+		ps.setString( 3, firstName.getText());									//set first name
+		ps.setString( 4, lastName.getText() );									//set last name
+		ps.setFloat( 5, bal );													// set initial balance
+		ps.executeUpdate();
 		
 		// close scene
+		conn.close();
 		Stage stageClose = (Stage) newAccountBttn.getScene().getWindow();
 		stageClose.close();
 		
--- cookie_project/src/userInterface/loginController.java
+++ cookie_project/src/userInterface/loginController.java
@@ -5,8 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import backend.dbConnect;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,6 +24,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -29,31 +38,67 @@ public class loginController implements Initializable {
 	@FXML TextField username;
 	@FXML PasswordField password;
 	@FXML Button new_acc;
-	String accNum;
 	
 	public void initialize(URL url, ResourceBundle rb){
 		username.setPromptText("Username");
 		password.setPromptText("Password");
-		accNum = "12345";
-		
 	}
 	
-	public void logIn(ActionEvent event) throws IOException {
+	public void logIn(ActionEvent event) throws IOException, SQLException {
+		
+		// authenticate user and password
+		dbConnect db = new dbConnect( );
+		Connection conn = db.connect( "sjshilts", "sJSdbPass10" );
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String stmt;
+		String dbPassword = "";
+		String passWord = password.getText();
+		try {
+		stmt = "SELECT password FROM Users WHERE username ='" + username.getText() + "'";
+		ps = conn.prepareStatement( stmt );
+		rs = ps.executeQuery();
+		rs.next();
+		dbPassword = rs.getString("password");
+		}
+		catch (SQLException e) {
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Incorrect username, password combination");
+			errorAlert.showAndWait();
+			return;
+		}
+		
+		if(!passWord.equals(dbPassword)){
+			Alert errorAlert = new Alert(AlertType.INFORMATION);
+			errorAlert.setHeaderText("Incorrect username, password combination");
+			errorAlert.showAndWait();
+			return;
+		}
+		
+		// open new scene
 		Parent root = FXMLLoader.load((getClass().getResource("userInterface.fxml")));
 		Scene scene = new Scene(root);
 		Stage stage = new Stage();
 		stage.setTitle("My Money Management...");
 		stage.getIcons().add(new Image("/images/cookie_icon.png"));
 		stage.setScene(scene);
-		stage.show();	
+		stage.show();
 		
+		// save account number
+		stmt = "SELECT Accnum FROM Users WHERE username ='" + username.getText() + "'";
+		ps = conn.prepareStatement( stmt );
+		rs = ps.executeQuery();
+		rs.next();
+		String accNum = rs.getString("Accnum");
 	    OutputStream targetStream = new FileOutputStream("src/userInterface/AccountNumber.txt");
 	    byte[] b = accNum.getBytes();
 	    targetStream.write(b);
 	    targetStream.close();
 		
+	    // close login screen and close database connection
 		Stage stageClose = (Stage) log_in.getScene().getWindow();
 		stageClose.close();
+		conn.close();
 	}
 	
 	public void newAccount(ActionEvent event) throws IOException{
