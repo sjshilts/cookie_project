96de3be56dc48245928321edafdd17eaeecaaa26
jkdeyonk <jkdeyonk@mtu.edu>
2019-03-27 21:29:47 -0400

    initialized array lists for dummyData

--- cookie_project/src/backend/dummyData.java
+++ cookie_project/src/backend/dummyData.java
@@ -1,10 +1,16 @@
 package backend;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 public class dummyData {
 	public static void main(String [] args) {
 		ArrayList<Integer> Accunum = new ArrayList<>();
+		ArrayList<Integer> type = new ArrayList<>();
+		ArrayList<Date> date = new ArrayList<>();
+		ArrayList<Integer> amount = new ArrayList<>();
+		ArrayList<Integer> who = new ArrayList<>();
+		
 	}
 
 }
