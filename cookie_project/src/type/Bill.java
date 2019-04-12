cf66f25b92557a599e45f6983283449904a19142
jkdeyonk <jkdeyonk@mtu.edu>
2019-04-12 11:48:38 -0400

    Finished commenting bill, and both css files

--- cookie_project/src/type/Bill.java
+++ cookie_project/src/type/Bill.java
@@ -1,25 +1,49 @@
 package type;
+import java.io.IOException;
 import java.sql.Date;
+import java.sql.SQLException;
 
 public class Bill extends Outflow{
 
-	
+	/**
+	 * 
+	 * Constructor for the Bill
+	 * @param dataEntry, amountEntry
+	 */
 	public Bill ( Date dateEntry, double amountEntry){
 		super( dateEntry, amountEntry );
 	}
 	
+	/**
+	 * 
+	 * Calls amountEntry and sets values for setAmount
+	 * @param amountEntry
+	 */
 	public void setAmount( double amountEntry ) {
 		super.setAmount(amountEntry);
 	}
 	
+	/**
+	 * 
+	 * Calls dataEntry and sets values for setDate
+	 * @param dataEntry
+	 */
 	public void setDate( Date dateEntry ) {
 		super.setDate(dateEntry);
 	}
 	
+	/**
+	 * 
+	 * Retrieves an amount
+	 */
 	public double getAmount() {
 		return super.getAmount();
 	}
 	
+	/**
+	 * 
+	 * Retrieves a date
+	 */
 	public Date getDate() {
 		return super.getDate();
 	}
