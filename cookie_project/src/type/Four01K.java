fd1e7b3151707ba9258ee41652aed88880704bbc
jkdeyonk <jkdeyonk@mtu.edu>
2019-04-15 11:52:43 -0400

    Commenting

--- cookie_project/src/type/Four01K.java
+++ cookie_project/src/type/Four01K.java
@@ -4,22 +4,45 @@ import java.sql.Date;
 
 public class Four01K extends Saving {
 
+	/**
+	 * 
+	 * Constructor for the Four01K
+	 * @param dataEntry, amountEntry
+	 */
 	public Four01K( Date dateEntry, double amountEntry){
 		super( dateEntry, amountEntry );
 	}
 	
+	/**
+	 * 
+	 * Calls amountEntry and sets values for setAmount
+	 * @param amountEntry
+	 */
 	public void setAmount( double amountEntry ) {
 		super.setAmount(amountEntry);
 	}
 	
+	/**
+	 * 
+	 * Calls dataEntry and sets values for setDate
+	 * @param dataEntry
+	 */
 	public void setDate( Date dateEntry ) {
 		super.setDate(dateEntry);
 	}
 	
+	/**
+	 * 
+	 * Retrieves an amount
+	 */
 	public double getAmount() {
 		return super.getAmount();
 	}
 	
+	/**
+	 * 
+	 * Retrieves a date
+	 */
 	public Date getDate() {
 		return super.getDate();
 	}
